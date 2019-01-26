b1f8e462f00754e053d4b715378bee1cf6131a74
= <=>
2019-01-26 23:47:25 +0100

    local

--- Pillow Fight/Assets/MainMenu/PlayerStats.cs
+++ Pillow Fight/Assets/MainMenu/PlayerStats.cs
@@ -9,13 +9,54 @@ public class PlayerStats : MonoBehaviour
     public GameObject pPrefab;
     public Color pColor;
     public int pIndex;
-   
+    public MainMenu mM;
+    public int selectInd;
+
+    public float counter;
+
+    public void Start()
+    {
+        mM = GameObject.Find("MainMenu").GetComponent<MainMenu>();
+
+    }
 
     public void Update()
     {
-        if (Input.GetButtonDown("TriggerRight" + playerNumber))
+        if (mM.SelectPan.activeSelf == true)
         {
+            if(counter <= 0)
+            {
+                counter = 1f;
+
+                float x = Input.GetAxis("HorizontalLeft" + playerNumber);
+
+                if (x > 0)
+                {
+                    Debug.Log("Hallo");
+
+                    pIndex++;
+                    if (pIndex >= mM.SelectPan.GetComponent<SelectMenu>().charSelectBtn.Count)
+                    {
+                        pIndex = 0;
+                    }
+                }
+            }
+            else
+            {
+                counter -= 1 * Time.deltaTime;
+            }
             
+            
+
+            if (Input.GetButtonDown("TriggerRight" + playerNumber))
+            {
+
+            }
+
+            mM.SelectPan.GetComponent<SelectMenu>().UpdateMenu();
         }
+
+        
+        
     }
 }
