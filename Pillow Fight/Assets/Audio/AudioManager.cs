f4d89b729e1d7539be02fbfe2b5d689d0651a3c7
Alessandro Famà <alessandro.fama@ymail.com>
2019-01-26 17:32:55 +0100

    Audio Scripts

---
+++ Pillow Fight/Assets/Audio/AudioManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public static AudioManager instance = null;
+    private FMOD.Studio.EventInstance music;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+
+            //Je nach dem ob wir mehr scenes haben
+            //DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+    }
+
+    public void MusicStart()
+    {
+
+        FMOD.Studio.PLAYBACK_STATE _music;
+        music.getPlaybackState(out _music);
+        if (_music != FMOD.Studio.PLAYBACK_STATE.PLAYING)
+        {
+            music = FMODUnity.RuntimeManager.CreateInstance(FMODPaths.MUSIC);
+            music.start();
+        }
+    }
+
+    public void MusicStop()
+    {
+        FMOD.Studio.Bus musicBus = FMODUnity.RuntimeManager.GetBus("bus:/Master/Music");
+        musicBus.stopAllEvents(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+
+    public void MusicStopImmediate()
+    {
+        FMOD.Studio.Bus musicBus = FMODUnity.RuntimeManager.GetBus("bus:/Master/Music");
+        musicBus.stopAllEvents(FMOD.Studio.STOP_MODE.IMMEDIATE);
+    }
+
+    public void SetParameterInt(FMOD.Studio.EventInstance instance, string parameter, int value)
+    {
+        FMOD.Studio.ParameterInstance _parameter;
+        instance.getParameter(parameter, out _parameter);
+
+        _parameter.setValue(value);
+    }
+
+    public void SetParameterFloat(FMOD.Studio.EventInstance instance, string parameter, float value)
+    {
+        FMOD.Studio.ParameterInstance _parameter;
+        instance.getParameter(parameter, out _parameter);
+
+        _parameter.setValue(value);
+    }
+}
---
+++ Pillow Fight/Assets/Audio/FMODPaths.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class FMODPaths
+{
+    // EVENTS
+    //      Characters
+    public static readonly string PEDDA_ATTACK = "event:/characters/pedda/pedda_attack";
+    public static readonly string TED_ATTACK = "event:/characters/ted/ted_attack";
+    public static readonly string JUSTIN_ATTACK = "event:/characters/justin/justin_attack";
+    public static readonly string POMPF_ATTACK = "event:/characters/pompf/pompf_attack";
+
+
+    //      Music
+    public static readonly string MUSIC = "event:/music/music";
+
+    //      VCAs
+    public static readonly string VCA_MUSIC = "vca:/Music";
+    public static readonly string VCA_SFX = "vca:/SFX";
+
+    // PARAMETERS
+
+
+
+    // SNAPSHOTS
+    public static readonly string GOT_HIT = "snapshot:/gotHit";
+}
