5cce4b3b2023c04fb1941d58434507a74d7b2364
Alessandro Famà <alessandro.fama@ymail.com>
2019-01-26 18:57:55 +0100

    push

--- Pillow Fight/Assets/Audio/PeddaSFXAnimationHelper.cs
+++ Pillow Fight/Assets/Audio/PeddaSFXAnimationHelper.cs
@@ -7,5 +7,6 @@ public class PeddaSFXAnimationHelper : MonoBehaviour
     public void PeddaFoosteps()
     {
         FMODUnity.RuntimeManager.PlayOneShot(FMODPaths.FOOTSTEPS_PEDDA, GetComponent<Transform>().position);
+
     }
 }
