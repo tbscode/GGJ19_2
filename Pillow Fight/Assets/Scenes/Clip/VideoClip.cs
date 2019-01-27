8a645a9fb9f5cace32e464c880de51db91e0b2d8
= <=>
2019-01-27 13:31:26 +0100

    local

---
+++ Pillow Fight/Assets/Scenes/Clip/VideoClip.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class VideoClip : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+
+    public void Pic1()
+    {
+        text.text = "Hey Sister, look! It's so cold outside. What should we do?";
+    }
+
+    public void Pic2()
+    {
+        text.text = "Don't worry. I have an idea!";
+    }
+
+    public void Pic3()
+    {
+        text.text = "Let's do some property damage with these things!";
+    }
+
+    public void EndScene()
+    {
+        text.text = "";
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Update()
+    {
+        if(Input.GetButtonDown("Submit"))
+        {
+            EndScene();
+        }
+    }
+
+    public void ClearText()
+    {
+        text.text = "";
+    }
+}
